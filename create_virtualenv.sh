bece903dfb2b177820772d367d9029b76b84e638
Erik Bjäreholt <erik.bjareholt@gmail.com>
2015-01-04 16:22:25 +0100

    Added travis config

--- create_virtualenv.sh
+++ create_virtualenv.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+
 sudo pip3 install virtualenv
 python3 -m virtualenv virtualenv
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 # TODO: Replace with requirements.txt or something
 
-. ./virtualenv/bin/activate
-pip3 install psutil --upgrade
-pip3 install pyuserinput --upgrade
-pip3 install git+https://github.com/LiuLang/python3-xlib.git --upgrade
-pip3 install git+https://github.com/ErikBjare/pyzenobase.git --upgrade
-deactivate
+pip install psutil --upgrade
+pip install pyuserinput --upgrade
+pip install git+https://github.com/LiuLang/python3-xlib.git --upgrade
+pip install git+https://github.com/ErikBjare/pyzenobase.git --upgrade
