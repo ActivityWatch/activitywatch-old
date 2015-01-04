1e9137d64bf1c66ee1d551332765cf2d69c7b043
Erik Bjäreholt <erik.bjareholt@gmail.com>
2015-01-04 12:12:31 +0100

    Added simple test

--- src/tests.py
+++ src/tests.py
@@ -1,1 +1,29 @@
-__author__ = 'erb'
+import unittest
+from datetime import datetime, timedelta
+
+from base import Watcher, Activity, Logger
+
+
+class MockWatcher(Watcher):
+    def __init__(self):
+        Watcher.__init__(self)
+
+
+class MockLogger(Logger):
+    def __init__(self):
+        Logger.__init__(self)
+
+
+class LoggerWatcherTest(unittest.TestCase):
+    def test_activity_flow(self):
+        watcher = MockWatcher()
+        logger = MockLogger()
+        logger.add_watcher(watcher)
+
+        watcher.add_activity(Activity("test", datetime.now()-timedelta(days=1), datetime.now()))
+
+        activities = logger.flush_activities()
+        self.assertTrue(len(activities) == 1)
+
+        activities = logger.flush_activities()
+        self.assertTrue(len(activities) == 0)
